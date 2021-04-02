e9450669d57008f763c7ca506d0e9c3423897465
Shawn Pinto <shawn@albanna-eng.com>
2021-04-02 15:34:53 +0400

    Migration added to create the Logs table

--- BlazorServerHangfire/Data/Migrations/20210402113419_01.Designer.cs
+++ BlazorServerHangfire/Data/Migrations/20210402113419_01.Designer.cs
@@ -4,14 +4,16 @@ using BlazorServerHangfire.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlazorServerHangfire.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210402113419_01")]
+    partial class _01
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
